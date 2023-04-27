1046ff24a81e8a3770e98f6c845973fa69539230
Koza_jp97@student.itstep.org <Koza_jp97@student.itstep.org>
2023-04-28 00:54:26 +0300

    Add project files.

---
+++ serialization_02.1/InvoiceForPaymentClass.cs
@@ -0,0 +1,71 @@
+﻿using System.Runtime.Serialization;
+using System;
+
+namespace Payment
+{
+    [Serializable]
+    class InvoiceForPayment : ISerializable
+    {
+        public static bool IsSerializable { get; set; } = true;
+
+        public char    CurrencySign      { get; set; }
+        public decimal PaymentForDay     { get; set; }
+        public ushort  PaymentDaysCount  { get; set; }
+        public decimal DayOfDelayPenalty { get; set; }
+        public ushort  DaysOfDelayCount  { get; set; }
+
+        public decimal AmountWithoutPenalties => PaymentForDay * PaymentDaysCount;
+        public decimal PenaltyAmount => DayOfDelayPenalty * DaysOfDelayCount;
+        public decimal TotalAmount => AmountWithoutPenalties + PenaltyAmount;
+
+        public InvoiceForPayment() { }
+        public InvoiceForPayment(decimal paymentForDay, ushort paymentDaysCount,
+                                 decimal dayOfDelayPenalty, ushort daysOfDelayCount,
+                                 char currencySign = '$')
+        {
+            CurrencySign = currencySign;
+            PaymentForDay = paymentForDay;
+            PaymentDaysCount = paymentDaysCount;
+            DayOfDelayPenalty = dayOfDelayPenalty;
+            DaysOfDelayCount = daysOfDelayCount;
+        }
+
+        private InvoiceForPayment(SerializationInfo info, StreamingContext context)
+        {
+            try
+            {
+                CurrencySign = info.GetChar("ВАЛЮТА");
+                PaymentForDay = info.GetDecimal("ОПЛАТА ЗА ДЕНЬ");
+                PaymentDaysCount = info.GetUInt16("К-СТЬ ДНІВ ОПЛАТИ");
+                DayOfDelayPenalty = info.GetDecimal("ШТРАФ ЗА ДЕНЬ ЗАТРИМКИ ОПЛАТИ");
+                DaysOfDelayCount = info.GetUInt16("К-СТЬ ДНІВ ЗАТРИМКИ ОПЛАТИ");
+            }
+            catch (SerializationException)
+            {
+                Console.WriteLine("\n(!) серіалізація об'єкту була заблокована.\n" +
+                                  "поля об'єкту отримали значення за замовчуванням\n");
+            }
+        }
+
+        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (IsSerializable)
+            {
+                info.AddValue("ВАЛЮТА", CurrencySign);
+                info.AddValue("ОПЛАТА ЗА ДЕНЬ", PaymentForDay);
+                info.AddValue("К-СТЬ ДНІВ ОПЛАТИ", PaymentDaysCount);
+                info.AddValue("ШТРАФ ЗА ДЕНЬ ЗАТРИМКИ ОПЛАТИ", DayOfDelayPenalty);
+                info.AddValue("К-СТЬ ДНІВ ЗАТРИМКИ ОПЛАТИ", DaysOfDelayCount);
+            }
+        }
+
+        public override string ToString()
+        {
+            return "[РАХУНОК ДЛЯ ОПЛАТИ]\n" +
+                   $"оплата за день         -> [{PaymentForDay}{CurrencySign}]\n" +
+                   $"к-сть днів оплати      -> |{PaymentDaysCount}|\n" +
+                   $"штраф за день затримки -> [{DayOfDelayPenalty}{CurrencySign}]\n" +
+                   $"к-сть днів затримки    -> |{DaysOfDelayCount}|";
+        }
+    }
+}
---
+++ serialization_02.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.IO;
+using System;
+
+using Payment;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        SoapFormatter formatter = new SoapFormatter();
+
+        InvoiceForPayment payment = new InvoiceForPayment(1000m, 14, 100m, 3);
+        InvoiceForPayment trueResult = null;
+        InvoiceForPayment falseResult = null;
+
+        Console.WriteLine("ОРИГІНАЛ:\n");
+        Console.WriteLine(payment);
+
+        using (Stream fStream = File.Create("true.soap"))
+            formatter.Serialize(fStream, payment);
+
+        InvoiceForPayment.IsSerializable = false;
+
+        using (Stream fStream = File.Create("false.soap"))
+            formatter.Serialize(fStream, payment);
+
+        Console.WriteLine("\n\nРЕЗУЛЬТАТ СЕРІАЛІЗАІЇ ЗІ ЗНАЧЕННЯМ ПОЛЯ [IsSerializable] \"true\":\n");
+
+        using (Stream fStream = File.OpenRead("true.soap"))
+            trueResult = formatter.Deserialize(fStream) as InvoiceForPayment;
+        
+        Console.WriteLine(trueResult);
+        Console.WriteLine("\n\nРЕЗУЛЬТАТ СЕРІАЛІЗАІЇ ЗІ ЗНАЧЕННЯМ ПОЛЯ [IsSerializable] \"false\":");
+
+        using (Stream fStream = File.OpenRead("false.soap"))
+            falseResult = formatter.Deserialize(fStream) as InvoiceForPayment;
+        
+        Console.WriteLine(falseResult);
+        Console.ReadKey();
+    }
+}
